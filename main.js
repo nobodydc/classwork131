6812a67e441f22fdcf5c584b9c640a2cbc2081d9
nobodydc <87460850+nobodydc@users.noreply.github.com>
2022-01-19 16:02:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-img = "";
+function setup() {}
 
-function setup(){
-    canvas = createCanvas(640,420);
-    canvas.center();
+function preload() {
+
 }
 
-function preload(){
-img = loadImage('dog_cat.jpg');
-}
+function draw() {
 
-function draw(){
-    image(img,0,0,640,420);
-    fill('#fc0303');
-    text("dog",120,30);
-    noFill();
-    stroke('#fc0303');
-    rect(102,13,400,300);
 }
